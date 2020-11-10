c0e5c24eacf3e7da49f4c127d5df304e5be44071
Andrew Bradley <cspotcode@gmail.com>
2020-11-10 15:01:56 -0500

    starting to implement yarn 2 compat

--- src/createJestConfig.ts
+++ src/createJestConfig.ts
@@ -1,4 +1,6 @@
 import { Config } from '@jest/types';
+import { createRequire } from 'module';
+import * as Path from 'path';
 
 export type JestConfigOptions = Partial<Config.InitialOptions>;
 
@@ -6,10 +8,23 @@ export function createJestConfig(
   _: (relativePath: string) => void,
   rootDir: string
 ): JestConfigOptions {
+
+  function resolveRelativeTo (dir: string, moduleSpecifier: string) {
+    const req = createRequire(dir);
+    try {
+      return req.resolve(moduleSpecifier);
+    } catch {
+      return null;
+    }
+  }
+  const jestLocation = resolveRelativeTo(Path.join(rootDir, 'file.js'), 'jest') || require.resolve('jest')
+  const jestCoreLocation = resolveRelativeTo(jestLocation, '@jest/core')!
+  const jestConfigLocation = resolveRelativeTo(jestCoreLocation, 'jest-config')!
+
   const config: JestConfigOptions = {
     transform: {
       '.(ts|tsx)$': require.resolve('ts-jest/dist'),
-      '.(js|jsx)$': require.resolve('babel-jest'), // jest's default
+      '.(js|jsx)$': resolveRelativeTo(rootDir, 'babel-jest') || resolveRelativeTo(jestConfigLocation, 'babel-jest')!, // jest's default
     },
     transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
